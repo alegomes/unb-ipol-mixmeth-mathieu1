df79b21d4ba5091c5ddcfac79986c9b551290990
Alexandre Gomes <alegomes@gmail.com>
2018-12-05 20:04:09 -0200

    x3_character_assessment

--- src/LavineGschwend2006tabela2.R
+++ src/LavineGschwend2006tabela2.R
@@ -12,6 +12,10 @@ library(foreign)
 filepath <- '/Users/alegomes/GDrive/2018/unb/ipol/disc\ métodos\ mistos/provas/1.\ Mathieu/data/anes1992por/anes1992.POR'
 anes1992 <- read.spss(filepath, to.data.frame=TRUE)
 
+#########################################################################
+# CANDIDATE EVALUATION
+#########################################################################
+
 # Estimates of the effects on summary candidate evaluation 
 # (the thermometer score for the Republican candidate (Bush) 
 # minus the thermometer score for the Democratic candidate (Clinton), 
@@ -20,6 +24,10 @@ feeling_thermometer_republican_candidate <- anes1992$V923305
 feeling_thermometer_democratic_candidate <- anes1992$V923306
 y_candidate_evaluation <- (as.numeric(feeling_thermometer_republican_candidate)/100 - 
                            as.numeric(feeling_thermometer_democratic_candidate)/100)[1:100]
+
+#########################################################################
+# ISSUE PROXIMITY
+#########################################################################
 
 # A single issue proximity score was constructed for each respondent 
 # in each election year by averaging all issues for which the respondent
@@ -108,6 +116,10 @@ for (j in 1:length(anes1992)) { # For each responder
 
 x1_issue_proximity <- issue_proximity[1:100]
 
+#########################################################################
+# PARTY IDENTIFICATION
+#########################################################################
+
 # [?] Nao esta claro de onde veio a informacao de 'party identification'
 #
 # Vamos considerar que a identificacao partidaria do sujeito tem a ver 
@@ -120,7 +132,67 @@ feeling_thermometer_democratic_party <- anes1992$V923317
 feeling_thermometer_republican_party <- anes1992$V923318
 x2_party_identification <- ifelse(as.numeric(feeling_thermometer_republican_party) < as.numeric(feeling_thermometer_democratic_party), "D", "R")[1:100]
 
-x3_character_assessment <- runif(100)
+#########################################################################
+# CHARACTER ASSESSMENT
+#########################################################################
+
+# In constructing character assessment scores, we used the ‘Experience and Ability’, 
+# ‘Leadership Qualities’, and ‘Personal Qualities’, ANES master code categories. 
+# (…) we excluded comments not related to candidate qualities, such as those related 
+# to issues, parties and groups (e.g., the master code categories ‘Domestic Issues’, 
+# ‘Foreign Issues’, ‘Group Connections’ and ‘Government Activity/Philosophy’).
+#
+# Comparative character assessment scores were constructed by the formula: 
+#
+# (Pr + Nd) - (Pd + Nr)
+#  
+# where 
+#    Pr and Pd represent the number of positive comments about the 
+#       Republican and Democratic candidates, respectively, and 
+#    Nr and Nd represent the number of negative comments about the 
+#       Republican and Democratic candidates.
+#
+#    [?] O que se considera como comentarios positivos? 
+#        Não esta claro se seriam os apontamentos nas 5 questoes 
+#        do tipo 'Reasons would vote for/against ...'. 
+#        Vamos considerar que sim.
+#       
+
+republican_positive_comments <- data.frame(vote_for_bush1=anes1992$V923110,
+                                           vote_for_bush2=anes1992$V923111,
+                                           vote_for_bush3=anes1992$V923112,
+                                           vote_for_bush4=anes1992$V923113,
+                                           vote_for_bush5=anes1992$V923114)
+
+republican_negative_comments <- data.frame(vote_against_bush1=anes1992$V923116,
+                                          vote_against_bush2=anes1992$V923117,
+                                          vote_against_bush3=anes1992$V923118,
+                                          vote_against_bush4=anes1992$V923119,
+                                          vote_against_bush5=anes1992$V923120)
+
+democratic_positive_comments <- data.frame(vote_for_clinton1=anes1992$V923122,
+                                           vote_for_clinton2=anes1992$V923123,
+                                           vote_for_clinton3=anes1992$V923124,
+                                           vote_for_clinton4=anes1992$V923125,
+                                           vote_for_clinton5=anes1992$V923126)
+
+democratic_negative_comments <- data.frame(vote_against_clinton1=anes1992$V923128,
+                                          vote_against_clinton2=anes1992$V923129,
+                                          vote_against_clinton3=anes1992$V923130,
+                                          vote_against_clinton4=anes1992$V923131,
+                                          vote_against_clinton5=anes1992$V923132)
+
+num_of_republican_positive_comments <- apply(republican_positive_comments, 1, function(x){sum(!is.na(x))})
+num_of_republican_negative_comments <- apply(republican_negative_comments, 1, function(x){sum(!is.na(x))})
+num_of_democratic_positive_comments <- apply(democratic_positive_comments, 1, function(x){sum(!is.na(x))})
+num_of_democratic_negative_comments <- apply(democratic_negative_comments, 1, function(x){sum(!is.na(x))})
+
+x3_character_assessment <- ((num_of_republican_positive_comments + num_of_democratic_negative_comments) -
+                           (num_of_democratic_positive_comments + num_of_republican_negative_comments))[1:100]
+
+#########################################################################
+# 
+#########################################################################
 
 # Also included in the OLS regressions are 
 # sex (female 􏰃 1; male 􏰃 0),
@@ -133,26 +205,65 @@ anes1992$gender[as.character(anes1992$V923089) == 'Female'] <- 1
 anes1992$gender[as.character(anes1992$V923089) == 'Male'] <- 0
 x4_female <- anes1992$gender[1:100]
 
+#########################################################################
+# 
+#########################################################################
+
 # race (white 􏰃 1; otherwise 􏰃 0), and
 anes1992$race[anes1992$V923085 == 'White'] <- 1
 anes1992$race[anes1992$V923085 != 'White'] <- 0
 x5_white <- anes1992$race[1:100]
 
+#########################################################################
+# 
+#########################################################################
+
 # the number of policy issues for which the respondent provided a valid opinion
 # TODO Qual eh a variavel?  
+
+#########################################################################
+# 
+#########################################################################
 
 # TODO Como ele codificou?!
 x6_education <- anes1992$V923090[1:100]
 
+#########################################################################
+# 
+#########################################################################
 
 x7_ideological_thinking <- runif(100)
+#########################################################################
+# 
+#########################################################################
 x8_political_knowledge <- runif(100)
+#########################################################################
+# 
+#########################################################################
 x9_ideological_thinking_character_assessment <- rnorm(100, mean=50, sd=10)
+#########################################################################
+# 
+#########################################################################
 x10_ideological_thinking_issue_proximity <- rnorm(100, mean=45, sd=10)
+#########################################################################
+# 
+#########################################################################
 x11_ideological_thinking_party_identification <- rnorm(100, mean=40, sd=10)
+#########################################################################
+# 
+#########################################################################
 x12_political_knowledge_character_assessment <- rnorm(100, mean=35, sd=10)
+#########################################################################
+# 
+#########################################################################
 x14_political_knowledge_issue_proximity <- rnorm(100, mean=30, sd=10)
+#########################################################################
+# 
+#########################################################################
 x13_political_knowledge_party_identification <- rnorm(100, mean=25, sd=10)
+#########################################################################
+# 
+#########################################################################
 x15_number_of_valid_policy_responses <- rnorm(100, mean=20, sd=10)
 
 model <- lm(y_candidate_evaluation~ x1_issue_proximity+
