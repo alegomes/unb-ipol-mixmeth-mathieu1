2bc721fee01a9862bfd944350728207327fadc48
Alexandre Gomes <alegomes@gmail.com>
2018-12-17 20:58:38 -0200

    variavel av_gov_fhc faked

--- src/samuels2004tabela3.R
+++ src/samuels2004tabela3.R
@@ -19,6 +19,10 @@ cat_emp_aposentado <- sample(rep(c(0,1),1000),1000)
 cat_emp_estudante <- sample(rep(c(0,1),1000),1000)
 cat_emp_desempregado <- sample(rep(c(0,1),1000),1000)
 
+preferencias <- factor(c(1,2,3,4,5,6), labels=c('pessimo', 'ruim', 'regular pra ruim', 'regular pra bom', 'bom', 'otimo'), ordered=TRUE)
+
+av_gov_fhc <- sample(rep(preferencias,1000),1000)
+
 logit <- glm(identificacao_partidaria ~  cat_emp_trabalhador_rural + 
                                          cat_emp_pequeno_burgues +
                                          cat_emp_dona_casa +
@@ -29,7 +33,8 @@ logit <- glm(identificacao_partidaria ~  cat_emp_trabalhador_rural +
                                          cat_emp_maodeobra_nao_especalizada +
                                          cat_emp_aposentado +
                                          cat_emp_estudante +
-                                         cat_emp_desempregado , 
-              family="binomial")
+                                         cat_emp_desempregado + 
+                                         av_gov_fhc
+              ,family="binomial")
 
 summary(logit)
