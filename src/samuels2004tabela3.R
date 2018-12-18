d93e9542d9e21abae4d6a5128ca6beefd9d15b7e
Alexandre Gomes <alegomes@gmail.com>
2018-12-18 07:02:33 -0200

    categoria_emprego transformada em factor

--- src/samuels2004tabela3.R
+++ src/samuels2004tabela3.R
@@ -7,35 +7,48 @@
 
 identificacao_partidaria <- sample(rep(as.factor(c('PT','PSDB', 'PMDB', 'PFL', 'Outro', 'Nenhuma')),1000),1000)
 
-cat_emp_trabalhador_rural <- sample(rep(c(0,1),1000),1000)
-cat_emp_pequeno_burgues <- sample(rep(c(0,1),1000),1000)
-cat_emp_dona_casa <- sample(rep(c(0,1),1000),1000)
-cat_emp_setor_informal <- sample(rep(c(0,1),1000),1000)
-cat_emp_profissional_liberal <- sample(rep(c(0,1),1000),1000)
-cat_emp_militar_policial <- sample(rep(c(0,1),1000),1000)
-cat_emp_maodeobra_especializada <- sample(rep(c(0,1),1000),1000)
-cat_emp_maodeobra_nao_especalizada <- sample(rep(c(0,1),1000),1000)
-cat_emp_aposentado <- sample(rep(c(0,1),1000),1000)
-cat_emp_estudante <- sample(rep(c(0,1),1000),1000)
-cat_emp_desempregado <- sample(rep(c(0,1),1000),1000)
+categoria_emprego <- factor(c('trabalhador rural', 'pequeno burgues', 'dona de casa', 'setor informal', 
+                            'profissional liberal', 'militar/policial', 'mao de obra especializada',
+                            'mao de obra nao especializada', 'aposentado', 'estudante',
+                            'desempregado'))
+
+categoria_emprego <- sample(rep(categoria_emprego,1000),1000)
+
+# cat_emp_trabalhador_rural <- sample(rep(c(0,1),1000),1000)
+# cat_emp_pequeno_burgues <- sample(rep(c(0,1),1000),1000)
+# cat_emp_dona_casa <- sample(rep(c(0,1),1000),1000)
+# cat_emp_setor_informal <- sample(rep(c(0,1),1000),1000)
+# cat_emp_profissional_liberal <- sample(rep(c(0,1),1000),1000)
+# cat_emp_militar_policial <- sample(rep(c(0,1),1000),1000)
+# cat_emp_maodeobra_especializada <- sample(rep(c(0,1),1000),1000)
+# cat_emp_maodeobra_nao_especalizada <- sample(rep(c(0,1),1000),1000)
+# cat_emp_aposentado <- sample(rep(c(0,1),1000),1000)
+# cat_emp_estudante <- sample(rep(c(0,1),1000),1000)
+# cat_emp_desempregado <- sample(rep(c(0,1),1000),1000)
 
 preferencias <- factor(c(1,2,3,4,5,6), labels=c('pessimo', 'ruim', 'regular pra ruim', 'regular pra bom', 'bom', 'otimo'), ordered=TRUE)
-
 av_gov_fhc <- sample(rep(preferencias,1000),1000)
+
 opiniao_sobre_lula <- sample(rep(factor(0:12, ordered=T),1000),1000)
 ideologia_esquerda_direita <- sample(rep(factor(c(0:10,66), ordered=T),1000),1000)
+religicao_catolico  <- sample(rep(factor(c(0:10,66), ordered=T),1000),1000)
+religicao_evangelico  <- sample(rep(factor(c(0:10,66), ordered=T),1000),1000)
 
-logit <- glm(identificacao_partidaria ~  cat_emp_trabalhador_rural + 
-                                         cat_emp_pequeno_burgues +
-                                         cat_emp_dona_casa +
-                                         cat_emp_setor_informal + 
-                                         cat_emp_profissional_liberal + 
-                                         cat_emp_militar_policial +
-                                         cat_emp_maodeobra_especializada + 
-                                         cat_emp_maodeobra_nao_especalizada +
-                                         cat_emp_aposentado +
-                                         cat_emp_estudante +
-                                         cat_emp_desempregado + 
+# TODO Se ligar que ele desmembra os factors na regressao!!!!
+# Avaliar o que eh variavel e o que eh (pode ser) factor
+
+logit <- glm(identificacao_partidaria ~  categoria_emprego+
+                                         # cat_emp_trabalhador_rural + 
+                                         # cat_emp_pequeno_burgues +
+                                         # cat_emp_dona_casa +
+                                         # cat_emp_setor_informal + 
+                                         # cat_emp_profissional_liberal + 
+                                         # cat_emp_militar_policial +
+                                         # cat_emp_maodeobra_especializada + 
+                                         # cat_emp_maodeobra_nao_especalizada +
+                                         # cat_emp_aposentado +
+                                         # cat_emp_estudante +
+                                         # cat_emp_desempregado + 
                                          av_gov_fhc +
                                          opiniao_sobre_lula +
                                          ideologia_esquerda_direita
