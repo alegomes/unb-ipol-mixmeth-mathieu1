30584c7c9e9a271728880173c872af71f83c5a2e
Alexandre Gomes <alegomes@gmail.com>
2018-12-19 15:05:29 -0200

    first commit

---
+++ src/samuels2004tabela4.R
@@ -0,0 +1,6 @@
+############################################################
+# Métodos Mistos - IPol/UnB - 2º/2018
+# Prof. Dr. Mathieu Turgeon
+# Reproducao da TABELA 4 do artigo Samuels (2004)
+# Alunos: Alexandre Gomes e Beatriz Franco
+############################################################
