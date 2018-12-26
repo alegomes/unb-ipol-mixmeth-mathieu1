25fcf62d02c764a59a1992989b3dd46bcd28a204
Alexandre Gomes <alegomes@gmail.com>
2018-12-26 11:37:47 -0200

    first commit

---
+++ src/samuels2004tabela5.R
@@ -0,0 +1,26 @@
+############################################################
+# Métodos Mistos - IPol/UnB - 2º/2018
+# Prof. Dr. Mathieu Turgeon
+# Reproducao da TABELA 5 do artigo Samuels (2004)
+# Alunos: Alexandre Gomes e Beatriz Franco
+############################################################
+
+library(pander)
+
+# (…) precisamos comparar o impacto relativo destas duas variáveis (opinião
+# sobre Lula e posicionamento ideológico esquerda-direita) (antes de continuar a
+# discutir as outras variáveis estatisticamente significativas) (…). O modo de
+# fazer essa comparação dá-se, primeiro, fixando todas as variáveis em seus
+# valores médios e obtendo a predição da linha-base. Depois, obtemos a
+# probabilidade de ser petista quando todas as variáveis da Tabela 4 são
+# colocadas em seus valores de “maior probabilidade de ser petista”. Ou seja,
+# podemos mandar o CLARIFY fixar “avaliação retrospectiva” em seu valor mínimo
+# (isto é, anti-FHC), a “opinião sobre Lula” em seu valor máximo, esquerdismo em
+# seu valor mínimo (isto é “mais esquerdista”) para todos os brasileiros e
+# depois pedir ao programa para predizer a probabilidade de um brasileiro com
+# aquelas características ser petista. Em seguida, podemos mudar os valores de
+# qualquer das variáveis a fim de medir o impacto relativo de mudar o valor de
+# uma determinada variável sobre a predição. A Tabela 5 apresenta vários
+# cenários. (Pg 235)
+
+
